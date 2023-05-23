b43f66b1dc1b41a76d3fff45d42fe745a630c143
陳金昌 Chris Chen <37694710+kyocoolcool@users.noreply.github.com>
2023-05-23 18:16:48 +0800

    Update hello-openshift-for-developers.go
    hello world

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello OpenShift for Developers!"
+		response = "Hello Word!"
 	}
 
 	fmt.Fprintln(w, response)
